2f1e3d23a2bea620eef980942bb1b7b2b770b04e
Siv <sivsivsree@gmail.com>
2021-05-17 11:34:08 +0400

    :tada: types defined.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,11 @@ export {
     StreamEvents,
     Stream
 } from './data'
+
+export {
+    IUserAppCode,
+    IResetData,
+    IAccountCreated,
+    IUserEmailUpdated,
+    IUserPhoneUpdated
+} from './interfaces'
