16b7b8dc0583d5516fc5138a5889c55e0dec1aa1
anas <anas@frsapp.com>
2021-05-17 14:52:24 +0400

    added sms,email events

--- src/data/StreamEvents.ts
+++ src/data/StreamEvents.ts
@@ -2,5 +2,7 @@ export enum StreamEvents {
     CUSTOMER_PASS_RESET = 'customerPasswordReset',
     CUSTOMER_CREATED = 'customerCreated',
     CUSTOMER_PHONE_UPDATED = 'customerPhoneUpdated',
-    CUSTOMER_EMAIL_UPDATED = 'customerEmailUpdated'
+    CUSTOMER_EMAIL_UPDATED = 'customerEmailUpdated',
+    DELIVER_SMS = 'sms',
+    DELIVER_EMAIL = 'email'
 }
