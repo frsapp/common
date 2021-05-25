a33d9ff52843e553bcf53efd73cf0657820002e2
Siv <sivsivsree@gmail.com>
2021-05-25 12:51:03 +0400

    :tada: added the template types.

--- src/templates/notification-center.ts
+++ src/templates/notification-center.ts
@@ -3,7 +3,6 @@ export const templates = {
         placeholder: {
             LINK: "ONBOARDING_URL_LINK"
         },
-        default: "Please find your onboard lint to reset password \n \n {{ONBOARD_URL_LINK}}"
+        default: "Please find your onboard lint to reset password \n \n {{ONBOARDING_URL_LINK}}"
     }
-
 };
