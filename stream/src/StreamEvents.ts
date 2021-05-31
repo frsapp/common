2f7820fbce39b06329e903fb1d36ed6375c1e1a1
anas <anas@frsapp.com>
2021-05-31 11:59:39 +0400

    added customer insurance renwal events

--- stream/src/StreamEvents.ts
+++ stream/src/StreamEvents.ts
@@ -3,6 +3,8 @@ export enum StreamEvents {
     CUSTOMER_CREATED = 'customerCreated',
     CUSTOMER_PHONE_UPDATED = 'customerPhoneUpdated',
     CUSTOMER_EMAIL_UPDATED = 'customerEmailUpdated',
+    CUSTOMER_INSURANCE_RENEWAL_EMAIL = 'customerInsuranceRenewalEmail',
+    CUSTOMER_INSURANCE_RENEWAL_SMS = 'customerInsuranceRenewalSMS',
     DELIVER_SMS = 'sms',
     DELIVER_EMAIL = 'email'
 }
