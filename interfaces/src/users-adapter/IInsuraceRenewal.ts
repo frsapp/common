6f95ec878867c9b80474dc77a1117f890228d461
anas <anas@frsapp.com>
2021-06-02 12:16:26 +0400

    added whatsapp event, insurance renewal interface

---
+++ interfaces/src/users-adapter/IInsuraceRenewal.ts
@@ -0,0 +1,8 @@
+export interface IInsuranceRenewal {
+    userId: string;
+    insuranceName: string;
+    insuranceExpiry: string;
+    insuranceLogo: string;
+    link: string;
+    vehicle: string;
+}
--- stream/src/StreamEvents.ts
+++ stream/src/StreamEvents.ts
@@ -5,6 +5,7 @@ export enum StreamEvents {
     CUSTOMER_EMAIL_UPDATED = 'customerEmailUpdated',
     CUSTOMER_INSURANCE_RENEWAL_EMAIL = 'customerInsuranceRenewalEmail',
     CUSTOMER_INSURANCE_RENEWAL_SMS = 'customerInsuranceRenewalSMS',
+    CUSTOMER_INSURANCE_RENEWAL_WHATSAPP = 'customerInsuranceRenewalWhatsapp',
     DELIVER_SMS = 'sms',
     DELIVER_EMAIL = 'email'
 }
