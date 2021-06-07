17486ef463a3de013ecbee0d94d0311f0e027b69
anas <anas@frsapp.com>
2021-06-07 11:56:43 +0400

    hotfix export

--- utils/src/index.ts
+++ utils/src/index.ts
@@ -1,10 +1,12 @@
-import {transformFilters} from './transformFilters';
-import {zip} from './zip';
-import {generateId, generatePassword} from './generateId';
+import { transformFilters } from './transformFilters';
+import { zip } from './zip';
+import { generateId, generatePassword } from './generateId';
 import loggerFactory from './logging';
-import {hashString} from './hashString';
-import {CorrelationIDHelper} from './correlationIDHelper'
-import {cleanupUndefinedKeys} from './cleanupUndefinedKeys';
+import { hashString } from './hashString';
+import { CorrelationIDHelper } from './correlationIDHelper'
+import { cleanupUndefinedKeys } from './cleanupUndefinedKeys';
+import { flattenArray } from './flattenArray';
+import { sleep } from './sleep';
 
 export {
     transformFilters,
@@ -14,5 +16,7 @@ export {
     loggerFactory,
     hashString,
     cleanupUndefinedKeys,
-    CorrelationIDHelper
+    CorrelationIDHelper,
+    flattenArray,
+    sleep
 };
