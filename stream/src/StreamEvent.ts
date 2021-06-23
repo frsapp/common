7bfd23b3a183f78b1654d8ac74949e8864a7ba68
anas <anas@frsapp.com>
2021-06-23 10:44:29 +0400

    hotfix: removed comments

--- stream/src/StreamEvent.ts
+++ stream/src/StreamEvent.ts
@@ -1,16 +1,12 @@
 export enum StreamEvent {
-    // customer stream
     CUSTOMER_CREATED = 'customerCreated',
     CUSTOMER_UPDATED = 'customerUpdated',
 
-    // renewal stream
     RENEWAL_STARTED = 'initiateCustomerRenewal',
 
-    //delivery stream
     DELIVER_SMS = 'sms',
     DELIVER_EMAIL = 'email',
     DELIVER_WHATSAPP = 'whatsapp',
 
-    // onboarding stream
     ONBOARDING_STARTED = 'intiateCustomerOnboarding'
 }
